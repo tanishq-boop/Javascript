30df2cba6824e1b07fd07b814f129d11deb961a5
tanishq-boop <tanishqagarwal1272@gmail.com>
2024-12-19 15:14:47 +0000

    l4

---
+++ basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+ 
+// console.log(null == 0); => false
+// console.log(null >= 0); => true
+
+//comparison(converts null to number 0 and 0>=0 is true aa 0=0) works differently from equality operator
+ console.log("2" === 2);
+ // === checks with datatype considered and does not type convert
+ 
+
+
+
+
+
+
