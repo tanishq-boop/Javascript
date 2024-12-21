a639f52c544385b5e1e242a75ff0137df7637037
tanishq-boop <tanishqagarwal1272@gmail.com>
2024-12-21 07:41:34 +0000

    mathinjs

---
+++ basics/06_numsandmaths.js
@@ -0,0 +1,43 @@
+// const score = 400
+// console.log(score);
+
+
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernumber = 123.546
+// console.log(othernumber.toPrecision(3)); // returns output in string
+ 
+// const hundreds =100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++
+//  console.log(Math);
+//  console.log(Math.abs(-5));
+//  console.log(Math.round(4.6));
+//  console.log(Math.ceil(4.5));
+//  console.log(Math.floor(4.5));
+//  console.log(Math.max(2,5,7,8));
+//  console.log(Math.min(2,5,7,8));
+ 
+ console.log(Math.random());
+ console.log(Math.floor(Math.random()*10+1)); // floor is used to give a integer or rounded off output
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));  //Very important for random value between a given range
+
+
+ 
+ 
+ 
+ 
+ 
+
+
+
+
