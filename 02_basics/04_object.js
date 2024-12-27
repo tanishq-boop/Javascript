12776d95f43dab1f11ed3223fb41db758726f1c3
tanishq-boop <tanishqagarwal1272@gmail.com>
2024-12-27 12:48:48 +0000

    obj2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,57 @@
+// const tinderuser = new Object()  // singelton object
+const tinderuser = {} // non singelton object
+
+tinderuser.id = "123abc"
+tinderuser.name = "Sammy"
+tinderuser.isLoggedin = false
+
+// console.log(tinderuser)
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullName : {
+        userName : {
+                    firstname : "Tanishq",
+                    lastname: "Agarwal"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userName.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+//const obj3 = Object.assign({},obj1,obj2)  // {} is the target object in which all other source objects are merged , if not given obj2 merges in obj1
+
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id : 1,
+        email : "t@gmail.com"
+    },
+    {
+        id : 2,
+        email : "t@gmail.com"
+    },
+    {
+        id : 3,
+        email : "t@gmail.com"
+    },
+    {
+        id : 4,
+        email : "t@gmail.com"
+    }
+]
+
+console.log(users[0].id);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+
