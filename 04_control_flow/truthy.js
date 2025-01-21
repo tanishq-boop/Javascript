f0dfef0ff6ca8b542846f519fe1a3ccba80e0031
tanishq-boop <tanishqagarwal1272@gmail.com>
2025-01-21 19:24:09 +0000

    .

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,12 @@
+const userEmail = "t@tanishq.ai"
+
+if(userEmail){
+    console.log("Got user Email");
+}  else {
+    console.log("Don't have user email");
+    
+}
+
+// falsy values  => 0 , BigInt(On) , -0 , false , "" , null , undefined , NaN
+
+//truthy values => "0" , " " , [] , {} , function(){}
