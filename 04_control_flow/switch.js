3adb312c206d8094c04561f09ea5ca625c6d72d0
tanishq-boop <tanishqagarwal1272@gmail.com>
2025-01-21 19:12:32 +0000

    0

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch(key){
+//     case value:
+
+//     break;
+
+//     default: 
+//     break;
+// }
+
+const month =3 
+switch(month){
+    case 1 : 
+    console.log("Jan");
+    break;
+    case 2 : 
+    console.log("Feb");
+    break;
+    case 3 : 
+    console.log("Mar");
+    break;
+    default : 
+    console.log("Default case Match ");
+    
+    
+}
