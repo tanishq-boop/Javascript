53fc4430af3e07f1a1609cb0c4a9ceb9af096799
tanishq-boop <tanishqagarwal1272@gmail.com>
2025-01-21 19:55:53 +0000

    .

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,4 +1,4 @@
-const userEmail = "t@tanishq.ai"
+const userEmail = []
 
 if(userEmail){
     console.log("Got user Email");
@@ -10,3 +10,38 @@ if(userEmail){
 // falsy values  => 0 , BigInt(On) , -0 , false , "" , null , undefined , NaN
 
 //truthy values => "0" , " " , [] , {} , function(){}
+
+
+
+// if(userEmail.length === 0){
+//     console.log("Array is Empty");
+    
+// }
+
+
+
+// const Obj = {}
+
+// if (Object.keys(Obj).length === 0 ) {
+//     console.log("Object is empty");
+    
+// }
+
+
+// Nullish coalescing operator (??) : null defined 
+
+let val1;
+//val1 = 5 ?? 10   // output => 5
+//val1 = null ?? 10 // output => 10
+//val1 = undefined ?? 15  // output =>15
+val1 = null ?? 10 ?? 20  //output => 10
+
+console.log(val1);
+
+//Terniary operators
+
+// condition ? true : false
+ const maggie = 40
+
+ maggie>=30 ? console.log("bhosdiwala") : console.log("Nice");
+  
