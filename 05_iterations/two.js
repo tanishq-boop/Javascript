e8fc0ae919488a16dc845d4ed1ed637f4fea01be
tanishq-boop <tanishqagarwal1272@gmail.com>
2025-01-21 20:30:32 +0000

    .

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// while
+let index = 0
+while (index<=10) {
+   // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let arr = ["batman", "supreman" , "flash"]
+let arrl = 0
+while (arrl<arr.length) {
+    //console.log(arr[arrl]);
+    arrl++
+}
+
+let score = 11 
+do {
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10)
